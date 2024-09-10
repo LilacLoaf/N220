9d1af7ab7a87cd2ad93a268ebd88bcf874ba3cd5
Jonathan Nguyen <nguyenjq@iu.edu>
2024-09-09 19:09:46 -0700

    Completed Homework 2

--- Homework-2/app.js
+++ Homework-2/app.js
@@ -1,1 +1,30 @@
-console.log(hi);
+let myName = "Jonathan";
+myName = "Not Jonathan";
+console.log(myName);
+//Do i have to do this everytime?
+document.getElementById("ticketNum").innerHTML += 1 * 3;
+document.getElementById("ticketCost").innerHTML += 14 * 1;
+document.getElementById("ttCost").innerHTML += " 14 * 3 = $" + 14 * 3;
+document.getElementById("money").innerHTML += 235.87 - 35 - 75 - 60;
+let jacket = "70";
+let first = "65.87";
+document.getElementById("funds").innerHTML += " " + !(first - jacket);
+// wasn't there supposed to be a video
+document.getElementById("profPizza").innerHTML +=
+  "4 pizzas times 8 slices =" + 4 * 8;
+document.getElementById("fed").innerHTML +=
+  "students fed: " + Math.floor(32 / 2.5);
+document.getElementById("prof").innerHTML +=
+  "Pizza for professor: " + (32 % 2.5);
+//stack overflow my beloved
+let adult = "12";
+let child = "6";
+let drink = "1.5";
+document.getElementById("Monty").innerHTML += 2 * +adult + +child + 3 * +drink;
+//lmao why is it called concatenate instead of append or something that's already used
+document.getElementById("totaltips").innerHTML +=
+  202.45 + 134.97 + 256.63 + 178.22;
+let tips = "772.27";
+document.getElementById("tips").innerHTML += "$" + tips / 4;
+//dont think the lecture went over an average command
+// not sure if i was suppoesd to look up how to round to hundredths
